2a474899de2b3f455cb8a462b91c6f3a6d8759ab
Sleepy-kookoo <americaxalvarez@gmail.com>
2024-04-05 19:27:42 -0400

    Answered Q10

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -32,7 +32,19 @@ const loopUpToNum = (num) => {
 //loopUpToNum(0)
 //loopUpToNum(-3)
 const fizzbuzz = () => {
+  for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz")
+    } else if (i % 3 === 0) {
+      console.log("fizz")
+    } else if (i % 5 === 0) {
+      console.log("buzz")
+    } else {
+      console.log(i)
+    }
+  }
 };
+fizzbuzz()
 
 module.exports = {
   loop0UpTo10,
