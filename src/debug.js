279c119bf94454bc46051ab2cf75a49cfc2a8720
Sleepy-kookoo <americaxalvarez@gmail.com>
2024-04-05 19:22:09 -0400

    Answered Q9

--- src/debug.js
+++ src/debug.js
@@ -5,17 +5,17 @@ const brokenLoop = (start, end) => {
   }
   return result;
 };
-console.log(brokenLoop(1, 10))
+//console.log(brokenLoop(1, 10))
 const brokenNested = () => {
   let result = '';
   for (let i = 0; i < 2; i++) {
-    for (let i = 0; i < 10; i++) {
-      result += `-${i}${i}`;
+    for (let a = 0; a < 10; a++) {
+      result += `-${i}${a}`;
     }
   }
   return result.slice(1);
 };
-
+console.log(brokenNested())
 module.exports = {
   brokenLoop,
   brokenNested,
